a92f4b683173c287e38aa146d9111b2e4c911e49
Civoremo <omerovic_n@hotmail.com>
2019-01-07 15:53:39 -0500

    created a branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// connect server with out application and use the responses to display data
+
 class App extends Component {
   render() {
     return (
